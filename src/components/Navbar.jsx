25d29bcc71d4a9292500d40a9c5bb5eb1f8aff6d
mJamandron21 <mjamandron@gmail.com>
2022-06-01 02:13:27 +0800

    Adding search bar in navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 const Navbar = () => {
   return (
     <Container>
-        Navbar
+      <Wrapper >
+        <Left>
+          <SearchContainer>
+            <SearchOutlinedIcon />
+            <Input/>
+          </SearchContainer>
+        </Left>
+        <Center>
+          Center
+        </Center>
+        <Right>
+          Right
+        </Right>
+      </Wrapper>
     </Container>
   )
 }
@@ -12,5 +26,47 @@ const Navbar = () => {
 export default Navbar
 
 const Container = styled.div`
-    
+  height: 4rem;
+  background-color: #DEA886;
+`;
+
+const Wrapper = styled.div`
+  padding: 0.8rem 2rem;
+  display: flex ;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid white;
+  background-color: transparent;
+  outline: none;
+  color: white;
+`; 
+
+const SearchContainer = styled.div`
+ border: none;
+ color: white;
+ display: flex;
+ align-items: center;
+ margin-left: 1rem;
+ padding: 0.5rem;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
 `;
