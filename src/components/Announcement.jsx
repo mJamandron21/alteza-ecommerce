8c765ca8dfc2d028a48d4d1f6933e10de4418c59
mJamandron21 <mjamandron@gmail.com>
2022-06-03 22:03:56 +0800

    Initial design for Announcement component

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const Announcement = () => {
   return (
-    <div>Announcement</div>
+   <Container>
+     Super Deals! Free Shipping on selected items!
+   </Container>
   )
 }
 
 export default Announcement
+
+const Container = styled.div`
+  height: 30px;
+  background-color:#908660; 
+  color: white;
+  text-align: center;
+`;
