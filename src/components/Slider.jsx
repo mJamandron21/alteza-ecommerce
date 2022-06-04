4303b7fb1f171d633b79a6d8d22ae9c7685ea0ee
mJamandron21 <mjamandron@gmail.com>
2022-06-05 00:34:00 +0800

    Adding Slider component

---
+++ src/components/Slider.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Slider = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default Slider
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+`;
