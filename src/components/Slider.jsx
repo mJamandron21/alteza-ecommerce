421c8683c132595861213ad0eff7b711b3106c7c
mJamandron21 <mjamandron@gmail.com>
2022-06-05 00:40:25 +0800

    Adding arrows for the Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,16 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material'
 import React from 'react'
 import styled from 'styled-components'
 
 const Slider = () => {
   return (
     <Container>
+        <Arrow>
+            <ArrowLeftOutlined />
+        </Arrow>
+        <Arrow>
+            <ArrowRightOutlined />
+        </Arrow>
 
     </Container>
   )
@@ -15,4 +22,11 @@ const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
+`;
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #fff7ff;
+    border-radius: 50%;
 `;
