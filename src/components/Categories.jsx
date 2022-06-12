bd9c551bb599221612a216c55031562be66ced1b
mJamandron21 <mjamandron@gmail.com>
2022-06-13 00:58:46 +0800

    Minor fix import styled-component.

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { styled } from '@material-ui/core';
+import styled from 'styled-components';
 import { categories } from '../data';
 import CategoryItem from './CategoryItem';
 
