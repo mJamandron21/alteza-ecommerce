4f9b4cf42560d88630972e155ea256993e58d3f2
mJamandron21 <mjamandron@gmail.com>
2022-06-15 01:47:55 +0800

    Initial design for category section.

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -7,7 +7,7 @@ const CategoryItem = ({item}) => {
       <Image src={item.img} alt='category-img'/>
       <Info>
         <Title>{item.title}</Title>
-        <Button>SHOP NOW</Button>
+        <Button>SHOW MORE</Button>
       </Info>
     </Container>
   )
@@ -16,21 +16,48 @@ const CategoryItem = ({item}) => {
 export default CategoryItem
 
 const Container = styled.div`
-
-`;
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+  `;
 
 const Image = styled.img`
-
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 40px;
 `;
 
 const Info = styled.div`
-
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-const Title = styled.div`
-
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
 `;
 
-const Button = styled.div`
-
+const Button = styled.button`
+  border: none;
+  background-color: black;
+  color: white;
+  font-size: large;
+  font-weight: bold;
+  padding: 10px;
+  cursor: pointer;
+    &:hover{
+        border: 1.5px solid black;
+        background-color: white;
+        color: black;
+        transition: all 0.5s ease-in-out;
+    }
 `;
