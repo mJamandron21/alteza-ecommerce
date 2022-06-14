e492bced2d79fbd1a75b7e657d3017f12ca07c38
mJamandron21 <mjamandron@gmail.com>
2022-06-15 00:57:13 +0800

    Change images that did not show on page.

--- src/data.js
+++ src/data.js
@@ -30,27 +30,28 @@ export const sliderItems = [
 export const categories = [
     {
         id: 1,
-        img: "https://www.pexels.com/photo/woman-wearing-blue-denim-jacket-2901680/",
+        // img: "https://www.pexels.com/photo/woman-wearing-blue-denim-jacket-2901680/",
+        img: "https://img.freepik.com/free-photo/fashionable-sensual-woman-black-leather-jacket-stylish-sunglasses-posing-rocks_273443-5237.jpg?t=st=1655225116~exp=1655225716~hmac=eedaedc564385525659ceb3fb23c7f722444301b2d05e025bb27741467da5824&w=740",
         title: "JACKETS 'ol HOODIES"
     },
     {
         id: 2,
-        img: "https://www.pexels.com/photo/woman-wearing-shades-while-kneeling-near-wooden-door-3672825/",
-        title: "COOL STREETWEARS"
-    },
-    {
-        id: 3,
-        img: "https://www.pexels.com/photo/woman-standing-bear-tall-grass-852860/",
+        img: "https://img.freepik.com/free-photo/graceful-blond-woman-stylish-summer-boho-outfit-posing-interior-studio_273443-5272.jpg?t=st=1655225206~exp=1655225806~hmac=ef511af0cc4e420e6dc8b62f0aec5760ad4f2bd6e27853a89398ef77efc6398f&w=740",
         title: "SUMMER VIBES"
     },
     {
+        id: 3,
+        img: "https://img.freepik.com/free-photo/attractive-casual-blond-girl-hat-dreamily-looking-away-while-walking-with-classic-bicycle-city-street_574295-4130.jpg?t=st=1655225513~exp=1655226113~hmac=640b29b9bafbccedb065e1145761d9655a9284177a12a39b2f206e1a7dea80e8&w=740",
+        title: "COOL STREETWEARS"
+    },
+    {
         id: 4,
-        img: "https://www.pexels.com/photo/smiling-woman-in-red-and-white-polka-dot-dress-3118650/",
+        img: "https://img.freepik.com/free-photo/stylish-tanned-girl-black-dress-red-neckerchief-posing-near-iron-lamppost_197531-3245.jpg?t=st=1655225748~exp=1655226348~hmac=9993addfa5baf1444e9aef1db5fcabed82987cd6271498ea6724421ecb7b17e2&w=740",
         title: "STYLISH DRESSES"
     },
     {
         id: 5,
-        img: "https://www.pexels.com/photo/a-side-view-of-a-woman-jogging-on-the-street-5310782/",
+        img: "https://img.freepik.com/free-photo/woman-lifting-dumbbells-gym_1303-31868.jpg?t=st=1655225340~exp=1655225940~hmac=eb6c45e00553cc4a6f7bc65e8fa04528fbbf2354b2970216af6fdc4d2ad54664&w=740",
         title: "SPORTY BABES"
     }
 ]
