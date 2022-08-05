a9267d958bb815b260a2e20a644eaad7e21f5d3b
mJamandron21 <mjamandron@gmail.com>
2022-08-05 12:46:06 +0800

    Minor fix.

--- src/data.js
+++ src/data.js
@@ -67,7 +67,7 @@ export const popularProducts = [
     },
     {
       id:3,
-      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      img:"https://pngimg.com/uploads/jacket/jacket_PNG8039.png",
     },
     {
       id:4,
